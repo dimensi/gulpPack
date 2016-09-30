e0df458b0b9e1a20c9f76d12c9783b16ea9e7cf5
Nikita Nafranets <eddimensi@gmail.com>
2016-09-30 15:42:00 +0300

    Правки + перевел все на табы + через beatufil пропустил.

--- gulpfile.js
+++ gulpfile.js
@@ -2,89 +2,89 @@
  * Галп плагины
  */
 
-var gulp         = require('gulp');
-var watch        = require('gulp-watch');
-var pug          = require('gulp-pug');
-var stylus       = require('gulp-stylus');
-var imagemin     = require('gulp-imagemin');
-var autoprefixer = require('gulp-autoprefixer');
-var plumber      = require('gulp-plumber');
-var tinypng      = require('gulp-tinypng-compress');
-var cleanCss     = require('gulp-clean-css');
-var changed      = require('gulp-changed');
-var concatCss    = require('gulp-concat-css');
-var rename       = require('gulp-rename');
-var sourcemaps   = require('gulp-sourcemaps');
-var gulpsync     = require('gulp-sync')(gulp);
+const gulp         = require('gulp');
+const watch        = require('gulp-watch');
+const pug          = require('gulp-pug');
+const stylus       = require('gulp-stylus');
+const imagemin     = require('gulp-imagemin');
+const autoprefixer = require('gulp-autoprefixer');
+const plumber      = require('gulp-plumber');
+const tinypng      = require('gulp-tinypng-compress');
+const cleanCss     = require('gulp-clean-css');
+const changed      = require('gulp-changed');
+const concatCss    = require('gulp-concat-css');
+const rename       = require('gulp-rename');
+const sourcemaps   = require('gulp-sourcemaps');
+const gulpsync     = require('gulp-sync')(gulp);
 
 /**
  * Другие плагины
  */
 
-var webpack          = require('webpack');
-var webpackStream   = require('webpack-stream');
-var webpackDevServer = require("webpack-dev-server");
-var webpackConfig    = require("./webpack.config.js");
-var rupture          = require('rupture');
-var rimraf           = require('rimraf');
-var browserSync      = require('browser-sync'),
-    reload           = browserSync.reload;
+const webpack          = require('webpack');
+const webpackStream    = require('webpack-stream');
+const webpackDevServer = require("webpack-dev-server");
+const webpackConfig    = require("./webpack.config.js");
+const rupture          = require('rupture');
+const rimraf           = require('rimraf');
+const browserSync      = require('browser-sync');
+const reload           = browserSync.reload;
 
-var path = {
-    assets: "./assets",
-    public: "./public",
+const path = {
+	assets: "./assets",
+	public: "./public",
 
-    vendorCss: [
-        './node_modules/normalize.css/normalize.css',
-        './assets/vendors/css/*.css'
-    ],
+	vendorCss: [
+		'./node_modules/normalize.css/normalize.css',
+		'./assets/vendors/css/*.css'
+	],
 
-    pugs: './assets/templates/*.pug',
+	pugs: './assets/templates/*.pug',
 
-    stylus: [
-        './assets/blocks/**/+(page|!*).styl'
-    ],
-    fonts: './assets/fonts/**/**.*'
+	stylus: [
+		'./assets/blocks/**/+(page|!*).styl'
+	],
+	fonts: './assets/fonts/**/**.*'
 };
 
-var onError = function (err) {
-    console.error('!ERROR!');
-    console.error(err);
+const onError = function(err) {
+	console.error('!ERROR!');
+	console.error(err);
 };
 
-var configs = {
-    //Standalone browser settings for browser-sync
-    browserConfig: {
-        server: {
-            baseDir: "./public/"
-        },
-        reloadDelay: 1500,
-        tunnel: false,
-        host: 'localhost',
-        port: 3000,
-        logPrefix: 'FrontEnd Server'
-    },
-    plumberError: {
-        errorHandler: onError
-    },
-    allowStreamReload: {
-        stream: true
-    },
-    pug: {
-        pretty: '\t'
-    },
+const configs = {
+	//Standalone browser settings for browser-sync
+	browserConfig: {
+		server: {
+			baseDir: "./public/"
+		},
+		reloadDelay: 1500,
+		tunnel: false,
+		host: 'localhost',
+		port: 3000,
+		logPrefix: 'FrontEnd Server'
+	},
+	plumberError: {
+		errorHandler: onError
+	},
+	allowStreamReload: {
+		stream: true
+	},
+	pug: {
+		pretty: '\t'
+	},
 
-    watch: {
-        fonts: path.fonts,
-        stylus: './assets/blocks/**/*.styl',
-        pugs: './assets/templates/*.pug',
-        pugsBlock: './assets/blocks/**/*.pug',
-        images: './assets/images/*.{png,jpg,jpeg}',
-        imagesBlock: './assets/blocks/**/*.{png,jpg,jpeg}',
-        svg: path.assets + '/images/**/*.{svg,gif}',
-        svgBlock: path.assets + '/blocks/**/*.{svg,gif}',
-        vendorsCss: './assets/vendors/css/*.css'
-    }
+	watch: {
+		fonts: path.fonts,
+		stylus: './assets/blocks/**/*.styl',
+		pugs: './assets/templates/*.pug',
+		pugsBlock: './assets/blocks/**/*.pug',
+		images: './assets/images/*.{png,jpg,jpeg}',
+		imagesBlock: './assets/blocks/**/*.{png,jpg,jpeg}',
+		svg: path.assets + '/images/**/*.{svg,gif}',
+		svgBlock: path.assets + '/blocks/**/*.{svg,gif}',
+		vendorsCss: './assets/vendors/css/*.css'
+	}
 };
 
 
@@ -92,7 +92,7 @@ var configs = {
  * Browser-sync сервер
  */
 gulp.task('browser-sync', function() {
-    browserSync.init(configs.browserConfig);
+	browserSync.init(configs.browserConfig);
 });
 
 
@@ -100,12 +100,15 @@ gulp.task('browser-sync', function() {
  * Собираем html из pug
  */
 gulp.task('pug', function() {
-    gulp.src(path.pugs)
-        .pipe(plumber(configs.plumberError))
-        .pipe(pug(configs.pug))
-        .pipe(changed(path.public, {extension: '.html', hasChanged: changed.compareSha1Digest}))
-        .pipe(gulp.dest(path.public))
-        .pipe(reload(configs.allowStreamReload));
+	gulp.src(path.pugs)
+		.pipe(plumber(configs.plumberError))
+		.pipe(pug(configs.pug))
+		.pipe(changed(path.public, {
+			extension: '.html',
+			hasChanged: changed.compareSha1Digest
+		}))
+		.pipe(gulp.dest(path.public))
+		.pipe(reload(configs.allowStreamReload));
 });
 
 
@@ -113,55 +116,51 @@ gulp.task('pug', function() {
  * Собираем css из styl
  */
 gulp.task('stylus', function() {
-    gulp.src(path.stylus)
-        .pipe(plumber(configs.plumberError))
-        .pipe(sourcemaps.init())
-        .pipe(stylus(
-            {
-                use: rupture(),
-                'import': [__dirname + '/assets/vendors/flex-grid-framework.styl']
-            }
-        ))
-        .pipe(autoprefixer({
-           browsers: ['> 1%', 'last 3 iOS versions', 'Firefox ESR', 'last 2 versions', 'iOS 8.1']
-       }))
-       .pipe(rename('style.css'))
-       .pipe(sourcemaps.write('./'))
-       .pipe(gulp.dest(path.public + '/css'))
-       .pipe(reload(configs.allowStreamReload));
+	gulp.src(path.stylus)
+		.pipe(plumber(configs.plumberError))
+		.pipe(sourcemaps.init())
+		.pipe(stylus({
+			use: rupture(),
+			'import': [__dirname + '/assets/vendors/flex-grid-framework.styl']
+		}))
+		.pipe(autoprefixer({
+			browsers: ['> 1%', 'last 3 iOS versions', 'Firefox ESR', 'last 2 versions', 'iOS 8.1']
+		}))
+		.pipe(rename('style.css'))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest(path.public + '/css'))
+		.pipe(reload(configs.allowStreamReload));
 });
 
 /**
  * Пришлось создать, из-за source-map ломалось все.
  */
 gulp.task('stylus:min', function() {
-    gulp.src(path.stylus)
-        .pipe(plumber(configs.plumberError))
-        .pipe(stylus(
-            {
-                use: rupture(),
-                'import': [__dirname + '/assets/vendors/flex-grid-framework.styl']
-            }
-        ))
-        .pipe(autoprefixer({
-           browsers: ['> 1%', 'last 3 iOS versions', 'Firefox ESR', 'last 2 versions', 'iOS 8.1']
-       }))
-       .pipe(rename('style.css'))
-       .pipe(cleanCss())
-       .pipe(gulp.dest(path.public + '/css'))
-       .pipe(reload(configs.allowStreamReload));
+	gulp.src(path.stylus)
+		.pipe(plumber(configs.plumberError))
+		.pipe(stylus({
+			use: rupture(),
+			'import': [__dirname + '/assets/vendors/flex-grid-framework.styl']
+		}))
+		.pipe(autoprefixer({
+			browsers: ['> 1%', 'last 3 iOS versions', 'Firefox ESR', 'last 2 versions', 'iOS 8.1']
+		}))
+		.pipe(rename('style.css'))
+		.pipe(cleanCss())
+		.pipe(gulp.dest(path.public + '/css'))
+		.pipe(reload(configs.allowStreamReload));
 });
 
 /**
  * Собираем стили от поставщиков
  */
 gulp.task('vendorCss', function() {
-    gulp.src(path.vendorCss)
-        .pipe(plumber(configs.plumberError))
-        .pipe(concatCss('all-styles.css'))
-        .pipe(cleanCss())
-        .pipe(gulp.dest(path.public + '/css'))
-        .pipe(reload(configs.allowStreamReload));
+	gulp.src(path.vendorCss)
+		.pipe(plumber(configs.plumberError))
+		.pipe(concatCss('all-styles.css'))
+		.pipe(cleanCss())
+		.pipe(gulp.dest(path.public + '/css'))
+		.pipe(reload(configs.allowStreamReload));
 });
 
 
@@ -174,27 +173,31 @@ gulp.task('css:min', ['stylus:min', 'vendorCss']);
 /**
  * Сжимаем svg отдельно
  */
- gulp.task('svg', function() {
-     gulp.src([path.assets + '/images/**/*.{svg,gif}', path.assets + '/blocks/**/*.{svg,gif}'])
-        .pipe(plumber(configs.plumberError))
- 		.pipe(imagemin({svgoPlugins: [{removeViewBox: false}]}))
-        .pipe(gulp.dest(path.public + '/images'))
-        .pipe(reload(configs.allowStreamReload));
- });
+gulp.task('svg', function() {
+	gulp.src([path.assets + '/images/**/*.{svg,gif}', path.assets + '/blocks/**/*.{svg,gif}'])
+		.pipe(plumber(configs.plumberError))
+		.pipe(imagemin({
+			svgoPlugins: [{
+				removeViewBox: false
+			}]
+		}))
+		.pipe(gulp.dest(path.public + '/images'))
+		.pipe(reload(configs.allowStreamReload));
+});
 
 
 /**
  * Сжимаем картинки через TinyPng
  */
 gulp.task('images', function() {
-    return gulp.src([path.assets + '/images/**/*.{png,jpg,jpeg}', path.assets + '/blocks/**/*.{png,jpg,jpeg}'])
-        .pipe(plumber(configs.plumberError))
-        .pipe(tinypng({
-             key: 'ZVe46i6n5KZuVPAqoHiHtIXt2QU--pxi',
- 			sigFile: './assets/.tinypng-sigs',
-             log: true
-         }))
-         .pipe(gulp.dest(path.assets + '/compressed'));
+	return gulp.src([path.assets + '/images/**/*.{png,jpg,jpeg}', path.assets + '/blocks/**/*.{png,jpg,jpeg}'])
+		.pipe(plumber(configs.plumberError))
+		.pipe(tinypng({
+			key: 'ZVe46i6n5KZuVPAqoHiHtIXt2QU--pxi',
+			sigFile: './assets/.tinypng-sigs',
+			log: true
+		}))
+		.pipe(gulp.dest(path.assets + '/compressed'));
 });
 
 
@@ -202,25 +205,27 @@ gulp.task('images', function() {
  * Объявил зависимость между задачами и перемещаю картинки
  */
 gulp.task('images:move', ['images'], function() {
-    gulp.src(path.assets + "/compressed/**/*")
-        .pipe(gulp.dest(path.public + '/images'))
-        .pipe(reload(configs.allowStreamReload));
+	gulp.src(path.assets + "/compressed/**/*")
+		.pipe(plumber(configs.plumberError))
+		.pipe(gulp.dest(path.public + '/images'))
+		.pipe(reload(configs.allowStreamReload));
 });
 
 /**
  * Общий таск на изображения
  */
-gulp.task('images:build', ['svg','images:move']);
+gulp.task('images:build', ['svg', 'images:move']);
 
 
 /**
  * Перемещаю шрифты
  */
- gulp.task('fonts', function() {
-     gulp.src(path.fonts)
-         .pipe(gulp.dest(path.public + '/fonts'))
-         .pipe(reload(configs.allowStreamReload));
- });
+gulp.task('fonts', function() {
+	gulp.src(path.fonts)
+		.pipe(plumber(configs.plumberError))
+		.pipe(gulp.dest(path.public + '/fonts'))
+		.pipe(reload(configs.allowStreamReload));
+});
 
 
 /**
@@ -228,58 +233,59 @@ gulp.task('images:build', ['svg','images:move']);
  */
 
 gulp.task('webpack', function() {
-   return gulp.src(path.assets + '/javascript/home.js')
-     .pipe(webpackStream(webpackConfig))
-     .pipe(gulp.dest(path.public));
- });
+	return gulp.src(path.assets + '/javascript/*.js')
+		.pipe(plumber(configs.plumberError))
+		.pipe(webpackStream(webpackConfig))
+		.pipe(gulp.dest(path.public));
+});
 
 /**
  * Очищаем папку
  */
 
 gulp.task('clean', function(cb) {
-    rimraf(path.public, cb);
+	rimraf(path.public, cb);
 });
 
 /**
  * Build
  */
-gulp.task('build', gulpsync.sync(['clean',['css:min', 'pug', 'fonts'], 'images:build']));
-gulp.task('final', gulpsync.sync(['clean',['css:min', 'pug', 'fonts'], 'images:build', 'webpack']));
+gulp.task('build', gulpsync.sync(['clean', ['css:min', 'pug', 'fonts'], 'images:build']));
+gulp.task('final', gulpsync.sync(['clean', ['css:min', 'pug', 'fonts'], 'images:build', 'webpack']));
 
 
 /**
  * Watch
  */
- gulp.task('watch', function() {
-     watch([configs.watch.stylus], function(event,cb) {
-         gulp.start('stylus');
-     });
-     watch([configs.watch.pugs], function(event,cb) {
-         gulp.start('pug');
-     });
-     watch([configs.watch.pugsBlock], function(event,cb) {
-         gulp.start('pug');
-     });
-     watch([configs.watch.fonts], function(event,cb) {
-         gulp.start('fonts');
-     });
-     watch([configs.watch.vendorsCss], function(event,cb) {
-         gulp.start('vendorCss');
-     });
-     watch([configs.watch.svg], function(event,cb) {
-         gulp.start('svg');
-     });
-     watch([configs.watch.svgBlock], function(event,cb) {
-         gulp.start('svg');
-     });
-     watch([configs.watch.images], function(event,cb) {
-         gulp.start('images:move');
-     });
-     watch([configs.watch.imagesBlock], function(event,cb) {
-         gulp.start('images:move');
-     });
- });
+gulp.task('watch', function() {
+	watch([configs.watch.stylus], function(event, cb) {
+		gulp.start('stylus');
+	});
+	watch([configs.watch.pugs], function(event, cb) {
+		gulp.start('pug');
+	});
+	watch([configs.watch.pugsBlock], function(event, cb) {
+		gulp.start('pug');
+	});
+	watch([configs.watch.fonts], function(event, cb) {
+		gulp.start('fonts');
+	});
+	watch([configs.watch.vendorsCss], function(event, cb) {
+		gulp.start('vendorCss');
+	});
+	watch([configs.watch.svg], function(event, cb) {
+		gulp.start('svg');
+	});
+	watch([configs.watch.svgBlock], function(event, cb) {
+		gulp.start('svg');
+	});
+	watch([configs.watch.images], function(event, cb) {
+		gulp.start('images:move');
+	});
+	watch([configs.watch.imagesBlock], function(event, cb) {
+		gulp.start('images:move');
+	});
+});
 
 /**
  * Default
--- webpack.config.js
+++ webpack.config.js
@@ -1,141 +1,139 @@
 "use strict";
 
-
-var NODE_ENV          = process.env.NODE_ENV || 'development';
-var path              = require('path');
-var webpack           = require('webpack');
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const path = require('path');
+const webpack = require('webpack');
 
 /** Пути */
-var assetsPath    = path.join(__dirname, 'assets');
-var publicPath    = path.join(__dirname, 'public');
-var blocksPath    = path.join(assetsPath, 'blocks');
-var imagesPath    = path.join(assetsPath, 'images');
-var jsPath        = path.join(assetsPath, 'javascript');
-var vendorsPath   = path.join(assetsPath, 'vendors');
-var vendorsJsPath = path.join(vendorsPath, 'js');
-var templatePath  = path.join(assetsPath, 'template');
-var fontsPath     = path.join(assetsPath, 'fonts');
+const paths = {
+	assets: path.join(__dirname, 'assets'),
+	public: path.join(__dirname, 'public'),
+	blocks: path.join(__dirname, 'assets', 'blocks'),
+	js: path.join(__dirname, 'assets', 'javascript'),
+	vendors: path.join(__dirname, 'assets', 'vendors', 'js')
+};
 
 
-var minChunks = 2;
+const minChunks = 2;
 
 module.exports = {
 
-    /**
-     * Общий путь
-     */
-    context: assetsPath,
+	/**
+	 * Общий путь
+	 */
+	context: paths.assets,
 
-    /**
-     * Точки входа
-     * Можно указать общий файл и в него, что-то общее добавить.
-     * common: "./javascript/common" и или можно включить в common другие файлы
-     * common: ["./javascript/welcome", "./javascript/common"]
-     */
-    entry: {
-        index: "index"
-    },
+	/**
+	 * Точки входа
+	 * Можно указать общий файл и в него, что-то общее добавить.
+	 * common: "./javascript/common" и или можно включить в common другие файлы
+	 * common: ["./javascript/welcome", "./javascript/common"]
+	 */
+	entry: {
+		index: "index"
+	},
 
-    /**
-     * Куда сохранять js
-     */
-    output: {
-        path: publicPath,
-        publicPath: '/js/', // паблик патч лучше его указазывать его разным для продакшен
-        filename: './js/[name].js',
-        library: '[name]',
-    },
+	/**
+	 * Куда сохранять js
+	 */
+	output: {
+		path: paths.public,
+		publicPath: '/js/', // паблик патч лучше его указазывать его разным для продакшен
+		filename: './js/[name].js',
+		library: '[name]',
+	},
 
-    /**
-     * Запускаю watch, если это разработка
-     */
-    watch: NODE_ENV == 'development',
+	/**
+	 * Запускаю watch, если это разработка
+	 */
+	watch: NODE_ENV == 'development',
 
-    /**
-     * Параметры для watch
-     */
-    watchOptions: {
-        aggregateTimeout: 150
-    },
+	/**
+	 * Параметры для watch
+	 */
+	watchOptions: {
+		aggregateTimeout: 150
+	},
 
-    /**
-     * Собираю source-map, разные при разных режимах
-     */
-    devtool: NODE_ENV == 'development' ? 'cheap-module-source-map' : 'source-map',
+	/**
+	 * Собираю source-map, разные при разных режимах
+	 */
+	devtool: NODE_ENV == 'development' ? 'cheap-module-source-map' : 'source-map',
 
-    /**
-     * Подключаю плагины
-     * 1. DefinePlugin, он передает всякие переменные в клиент
-     * 2. NoErrorsPlugin - не ломает webpack при ошибках
-     * 3. CommonsChunkPlugin - собирает из всех файлов один общий.
-     * Если указать chunks: ['about', 'home']
-     * дублировать плагин до тех пор, пока не надоест, то можно будет осознанно собирать общее с разных страниц.
-     * 4. ProvidePlugin подключение глобальные библиотек
-     */
-    plugins: [
-        new webpack.DefinePlugin({
-            NODE_ENV: JSON.stringify(NODE_ENV)
-        }),
-        new webpack.NoErrorsPlugin(),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "common",
-            minChunks: minChunks
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery/dist/jquery.min',
+	/**
+	 * Подключаю плагины
+	 * 1. DefinePlugin, он передает всякие переменные в клиент
+	 * 2. NoErrorsPlugin - не ломает webpack при ошибках
+	 * 3. CommonsChunkPlugin - собирает из всех файлов один общий.
+	 * Если указать chunks: ['about', 'home']
+	 * дублировать плагин до тех пор, пока не надоест, то можно будет осознанно собирать общее с разных страниц.
+	 * 4. ProvidePlugin подключение глобальные библиотек
+	 */
+	plugins: [
+		new webpack.DefinePlugin({
+			NODE_ENV: JSON.stringify(NODE_ENV)
+		}),
+		new webpack.NoErrorsPlugin(),
+		// new webpack.optimize.CommonsChunkPlugin({
+		//     name: "common",
+		//     minChunks: minChunks
+		// }),
+		new webpack.ProvidePlugin({
+			$: 'jquery/dist/jquery.min',
 			jQuery: 'jquery/dist/jquery.min',
 			"window.jQuery": 'jquery/dist/jquery.min'
-        }),
-    ],
+		})
+	],
 
-    /**
-     * Библиотеки с CDN
-     */
+	/**
+	 * Библиотеки с CDN
+	 */
 
-    // externals: {
-    //     jQuery: '$'
-    // },
+	// externals: {
+	//     jQuery: '$'
+	// },
 
 
-    /**
-     * Указываю пути где надо искать
-     * По моему я нахимичил с root..., оно работает как-то не так, как я хочу
-     * TODO: ПРОВЕРЬ!
-     */
-    resolve: {
-        root: [jsPath, vendorsJsPath, blocksPath],
-        modulesDirectories: ['node_modules'],
-        extensions: ['', '.js',]
-    },
+	/**
+	 * Указываю пути где надо искать
+	 * По моему я нахимичил с root..., оно работает как-то не так, как я хочу
+	 * TODO: ПРОВЕРЬ!
+	 */
+	resolve: {
+		root: [paths.js, paths.vendors, paths.blocks],
+		alias: {
+			jQuery: "jquery/dist/jquery.min"
+		},
+		modulesDirectories: ['node_modules'],
+		extensions: ['', '.js', ]
+	},
 
-    /**
-     * Указываю пути для лоадеров
-     */
-    resolveLoader: {
-        modulesDirectories: ['node_modules'],
-        moduleTemplates: ['*-loader', '*'],
-        extensions: ['', '.js']
-    },
+	/**
+	 * Указываю пути для лоадеров
+	 */
+	resolveLoader: {
+		modulesDirectories: ['node_modules'],
+		moduleTemplates: ['*-loader', '*'],
+		extensions: ['', '.js']
+	},
 
-    /**
-     * Подключаю лоадеры
-     * 1. Babel
-     */
-    module: {
+	/**
+	 * Подключаю лоадеры
+	 * 1. Babel
+	 */
+	module: {
 
-        loaders: [
-            {
-                test: /\.js$/,
-                include: [ jsPath, blocksPath],
-                loader: 'babel',
-                query: {
-                    presets: ['es2015'],
-                    plugins: ['transform-runtime']
-                }
-            },
-        ]
+		loaders: [{
+			test: /\.js$/,
+			include: [paths.js, paths.blocks],
+			loader: 'babel',
+			query: {
+				presets: ['es2015'],
+				plugins: ['transform-runtime']
+			}
+		}, ]
 
-    }
+	}
 };
 
 
@@ -143,14 +141,14 @@ module.exports = {
  * Если продакшен, то сжимаю
  */
 if (NODE_ENV == 'production') {
-  module.exports.plugins.push(
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          // don't show unreachable variables etc
-          warnings:     false,
-          drop_console: true,
-          unsafe:       true,
-        }
-      })
-  );
+	module.exports.plugins.push(
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				// don't show unreachable variables etc
+				warnings: false,
+				drop_console: true,
+				unsafe: true,
+			}
+		})
+	);
 }
