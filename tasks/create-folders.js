7703e58acd3644f419ac07e5c9716779a06b759c
Nikita Nafranets <eddimensi@gmail.com>
2016-12-06 00:27:13 +0300

    штучка для создания папок

---
+++ tasks/create-folders.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const mkdir = require('mkdirp');
+const notifier = require('node-notifier');
+
+const dirs = [
+	'./app/assets/fonts',
+	'./app/images',
+	'./app/vendors/css',
+	'./app/vendors/js'
+];
+
+gulp.task('mkdirs', function() {
+	dirs.forEach(function(dir) {
+		mkdir(dir);
+	});
+	return notifier.notify({
+		title: 'Папки',
+		message: 'Созданы'
+	});
+});
