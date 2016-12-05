cd34d9fcf7ba3c765923cb0011776b7f77d06895
Nikita Nafranets <eddimensi@gmail.com>
2016-12-05 19:59:47 +0300

    Починил gulp-zip
    и дату выставил

--- tasks/zip.js
+++ tasks/zip.js
@@ -6,20 +6,20 @@ const correctNumber = number => number < 10 ? '0' + number : number;
 
 const getDateTime = () => {
 	const now = new Date();
-	const year = now.getFullYear();
+	const year = (''.slice.call(now.getFullYear())).slice(-2);
 	const month = correctNumber(now.getMonth() + 1);
 	const day = correctNumber(now.getDate());
 	const hours = correctNumber(now.getHours());
 	const minutes = correctNumber(now.getMinutes());
 
-	return `${year}-${month}-${day}-${hours}${minutes}`;
+	return `${year}-${month}-${day}_${hours}-${minutes}`;
 };
 
 gulp.task('zip', () => {
-	const datetime = process.env.ZIP_DATE_TIME ? '-' + getDateTime() : '';
+	const datetime = '-' + getDateTime();
 	const zipName = 'dist' + datetime + '.zip';
 
-	gulp.src(['../public/**/*'])
+	gulp.src([path.join(__dirname, '..') + '/public/**/*'])
 		.pipe(zip(zipName))
-		.pipe(gulp.dest(path.join(__dirname, '..')));
+		.pipe(gulp.dest('.'));
 });
