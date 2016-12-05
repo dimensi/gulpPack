19079bd4f9788343d8b196524d4c688f121a8600
Nikita Nafranets <eddimensi@gmail.com>
2016-12-06 00:37:33 +0300

    Убрал лишний clean

--- gulpfile.js
+++ gulpfile.js
@@ -6,4 +6,4 @@ requireDir('./tasks');
 gulp.task('build', gulpsync.sync(['clean', ['stylus', 'vendorCss', 'pug', 'assets', 'images']]));
 gulp.task('build:final', gulpsync.sync(['clean', ['pug', 'stylus:min', 'vendorCss:min', 'assets'], 'images:build', 'webpack']));
 
-gulp.task('default', gulpsync.sync(['clean', 'build', 'watch', ['webpack', 'browser-sync']]));
+gulp.task('default', gulpsync.sync(['build', 'watch', ['webpack', 'browser-sync']]));
